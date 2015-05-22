8c4f409a1d9e7846d7911d11882912bf9a275530
angelozerr <angelo.zerr@gmail.com>
2015-05-22 18:38:30 +0200

    Try to fix build?

--- test/all.js
+++ test/all.js
@@ -1,4 +1,4 @@
 // dojo 1.10 tests
-exports['test dojo/dom (1.10)'] = require('./1.10/test_DojoDom');
+exports['test dojo/dom (1.10)'] = require('./1.10/test_DojoDom.js');
 
 if (require.main === module) require("test").run(exports);
