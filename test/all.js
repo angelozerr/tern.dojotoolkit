d25c2bfaf2791b17548500031954036572b98d84
angelozerr <angelo.zerr@gmail.com>
2015-05-23 03:11:36 +0200

    Fix build!

--- test/all.js
+++ test/all.js
@@ -1,4 +1,4 @@
 // dojo 1.10 tests
-exports['test dojo/dom (1.10)'] = require('./test_DojoDom');
+exports['test dojo/dom (1.10)'] = require('./1.10/test_dojoDom');
 
 if (require.main === module) require("test").run(exports);
